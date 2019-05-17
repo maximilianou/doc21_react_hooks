a78e2cb00a510c2bca8dc52253eb6676d673f450
maximilianou@gmail.com <maximilianou@gmail.com>
2019-05-17 11:57:08 -0300

    mocha test running empty

--- v1_hooks/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const elem = document.createElement('header');
-  ReactDOM.render(<App />, elem);
-  ReactDOM.unmountComponentAtNode(div);
-});
---
+++ v1_hooks/test/App.test.js
@@ -0,0 +1,5 @@
+describe('Initial test ', () => {
+  it('renders without crashing', () => {
+
+  });
+});
